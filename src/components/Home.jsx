1e23eae54a36900357f7f5e0f3e3000edfbe0d8f
LoopFruits <Kensworldc@gmail.com>
2022-11-18 16:33:58 -0500

    added color to portfolio button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
             <div className="flex flex-col justify-center h-full">
                 <h2 className="text-4xl sm:text-7xl font-bold text-white"> I'm a Font End Developer</h2>
-                <p className="text-gray-500 py-4"> 
+                <p className="text-gray-500 py-4 max-w-md"> 
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam, omnis!
                 </p>
                 <div>
-                    <button>
+                    <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-pink-500 to-orange-500 cursor-pointer">
                         Portfolio 
                         <span>
                             <MdOutlineArrowRightAlt />
