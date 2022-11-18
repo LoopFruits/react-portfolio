597f557a81341a9fc3472e7af2741f6367ae4281
LoopFruits <Kensworldc@gmail.com>
2022-11-18 14:42:02 -0500

    profile picture import sucess

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfilePic from '../images/Profilepic.jpg'
+
+const Home = () => {
+  return (
+    <div>
+        <img src={ProfilePic} alt="Kenneth Jordan" />
+    </div>
+  )
+}
+
+export default Home
