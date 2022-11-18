67240130734927f2635860c8df6ec540011c5073
LoopFruits <Kensworldc@gmail.com>
2022-11-18 16:27:07 -0500

    changed background color to sky blue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import ProfilePic from '../images/Profilepic.jpg'
+import ProfilePic from '../images/Profilepic.jpg';
+import { MdOutlineArrowRightAlt } from 'react-icons/md'
 
 const Home = () => {
   return (
-    <div>
-        <img src={ProfilePic} alt="Kenneth Jordan" />
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black to-sky-500 text-white">
+        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+            <div className="flex flex-col justify-center h-full">
+                <h2 className="text-4xl sm:text-7xl font-bold text-white"> I'm a Font End Developer</h2>
+                <p className="text-gray-500 py-4"> 
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam, omnis!
+                </p>
+                <div>
+                    <button>
+                        Portfolio 
+                        <span>
+                            <MdOutlineArrowRightAlt />
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div>
+                <img src={ProfilePic} alt="Kenneth Jordan's" 
+                className="rounded-2xl mx-auto w-2/3 md:w-full"></img>
+            </div>
+        </div>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,7 +28,7 @@ const NavBar = () => {
     },
   ]
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-blue fixed">
       <div>
           <h1 className="text-5xl font-signature ml-2">Kenneth</h1>
       </div>
