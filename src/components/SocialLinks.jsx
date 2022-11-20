e6c5006bcd6dd9d0e0dba2d1fbf44bd27a5011b3
LoopFruits <Kensworldc@gmail.com>
2022-11-19 20:37:47 -0500

    added linkedin social

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {FaGithub, FaLinkedin} from 'react-icons/fa';
+import {HiOutlineMail} from 'react-icons/hi';
+import {BsFillPersonalLinesFill} from 'react-icons/bs';
+
+
 
 const SocialLinks = () => {
-  return <div>SocialLinks</div>
-  
+  return ( 
+    <div className="flex flex-col top-[35%] left-0 fixed">
+        <ul>
+            <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-black">
+                <a 
+                href="https://www.linkedin.com/in/kenneth-the-dev/" 
+                className=" flex justify-between items-center w-full text-white"> 
+                    <>
+                        LinkedIn <FaLinkedin size={30}/>        
+                    </>
+                </a>
+            </li>
+        </ul>
+    </div>
+  );
 }
 
 export default SocialLinks
