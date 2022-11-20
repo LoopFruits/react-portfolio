167e83f313e37c4b8754679493f7e8ad285b19a0
LoopFruits <Kensworldc@gmail.com>
2022-11-20 14:54:38 -0500

    added styling to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div name="about" className="w-full h-screen bg-gradient-to-b from-sky-500 to-violet-500 text-white">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="pb-8">
+                <p className="text-4xl font-bold inline border-b-5 border-black">About</p>
+            </div>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora rerum officiis recusandae harum atque est possimus libero assumenda vitae quae molestias repudiandae nihil, nobis voluptates magnam esse. Suscipit, saepe voluptatem. Reprehenderit a amet, officiis sapiente cumque ab perspiciatis illo molestiae expedita ipsam soluta labore doloremque tempore, cupiditate fugiat nihil?
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tenetur, praesentium earum sapiente incidunt maiores enim in vitae odio adipisci, porro, quia aperiam perferendis pariatur blanditiis aliquam placeat autem hic deserunt dolor voluptatem unde quam optio illo. Dolorem incidunt perferendis facilis laboriosam maxime! Nostrum rem unde mollitia ipsam facere molestias.
+            </p>
+        </div>
+    </div>
   )
 }
 
