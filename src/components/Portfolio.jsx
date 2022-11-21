06a58f58111e1dfa70e50cce3c7575f269a5ddd0
LoopFruits <Kensworldc@gmail.com>
2022-11-21 11:31:14 -0500

    added images for portfolio

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PokeFinder from '../images/PokeFinder.png'
+import MentalHealthWithoutBorders from '../images/MentalHealthWithoutBorders.png'
+
+
+const Portfolio = () => {
+  return (
+    <div>
+        <div>
+            <p> Portfolio</p>
+            <p>Check out some of my work</p>
+        </div>
+        <div>
+            <div>
+                <img src="" alt=""></img>
+                <div>
+                    <button></button>
+                    <button></button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Portfolio
