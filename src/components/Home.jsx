4d583f20aa462a26150d9a55d8165e0e394932b3
LoopFruits <Kensworldc@gmail.com>
2022-12-24 19:06:40 -0500

    added animation to downward arrow

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProfilePic from '../images/Profilepic.jpg';
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import { MdOutlineArrowDownward } from 'react-icons/md'
 import {Link} from 'react-scroll'
 
 const Home = () => {
@@ -18,8 +18,8 @@ const Home = () => {
                         className="group transition ease-in-out delay-150 group-hover:rotate-90 hover:-translate-y-1 hover:scale-110 duration-300 text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-pink-500 to-orange-500 cursor-pointer">
                         Portfolio 
                         
-                        <span className="hover:rotate-90 duration-300">
-                            <MdOutlineKeyboardArrowRight size={30} className="ml-1"/>
+                        <span className="hover:rotate-90 animate-bounce duration-300">
+                            <MdOutlineArrowDownward size={30} className="ml-1"/>
                         </span>
                     </Link>
                 </div>
