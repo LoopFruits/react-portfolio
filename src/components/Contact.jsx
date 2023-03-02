fcff67948b883563e7553de982447594cb039da8
Kenneth Jordan <kensworldc@gmail.com>
2023-03-02 10:13:03 -0500

    fixing spacing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,7 +30,8 @@ const Contact = () => {
                 rows="10" 
                 placeholder="Enter your message"
                 className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-                ></textarea>
+                >
+                </textarea>
 
                 <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
                   Let's talk
