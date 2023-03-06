92708767fd1225eaebe0bf19b612847bc936a8c2
Kenneth Jordan <kensworldc@gmail.com>
2023-03-06 12:32:24 -0500

    added drop shadow to buttons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
                 >
                 </textarea>
 
-                <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+                <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300 drop-shadow-md hover:drop-shadow-xl">
                   Let's talk
                 </button>
               </form>
