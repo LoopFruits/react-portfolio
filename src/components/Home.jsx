72f8dfc32099c547d0381057c564a5e70aa33e2c
Kenneth Jordan <kensworldc@gmail.com>
2023-03-06 13:43:12 -0500

    fixed h2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-cyan-500 text-white">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
             <div className="flex flex-col justify-center h-full">
-                <h2 className="text-4xl sm:text-7xl font-bold text-white"> I'm a Font End Developer</h2>
+                <h2 className="text-4xl sm:text-7xl font-bold text-white"> I'm a Junior Developer</h2>
                 <p className="text-gray-500 py-4 max-w-md"> 
                     Junior Software Engineer. I love to work on web applications using technologies such as JavaScript, ReactJS, Ruby on Rails, TailwindCSS and PostgreSQL.
                 </p>
