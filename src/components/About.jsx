f0dafcebf1dce533d564075756caee9ac55db5a8
LoopFruits <Kensworldc@gmail.com>
2023-03-06 19:50:19 -0500

    edited the about me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
                 <p className="text-4xl font-bold inline">About</p>
             </div>
             <p className="text-xl mt-20">
-            New York based engineer with a background in Kinesiology.Experienced in JavaScript and Ruby on Rails based programming.I Possess strong skills in communication and teamwork that help companies drive positive results with innovative and creative ideas. Passionate about learning and staying up to date with tech.
+            As an experienced engineer with a background in Kinesiology, I have a strong foundation in problem-solving and analytical thinking. With expertise in JavaScript and Ruby on Rails based programming, I'm able to develop software solutions that drive positive results for companies. I possess excellent communication and teamwork skills that enable me to collaborate with colleagues to create innovative and creative ideas. As a lifelong learner, I'm passionate about staying up to date with the latest tech trends to create software that meets user needs.
             </p>
             <br />
             {/* <p className="text-xl">
