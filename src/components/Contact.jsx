c1f278db925d03d744cb9fd10d63dccddcdb2de3
Kenneth Jordan <kensworldc@gmail.com>
2023-03-07 12:32:28 -0500

    fixed contact component spacing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
         <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
             <div className="pb-8">
                 <p className="text-4xl font-bold inline ">Contact</p>
-                <p className="py-6"> Submit the form below to get in touch with me </p>
+                <p className="py-6">Submit the form below to get in touch with me </p>
             </div>
             <div className="flex justify-center items-center"> 
               <form action="https://getform.io/f/bb7915ea-48a0-4a68-95b7-9aaa23511436" method="POST" className="flex flex-col w-full md:w-1/2">
