5a49badf8eaaf2979a02cfdf6eb7ce39873edc5d
Kenneth Jordan <kensworldc@gmail.com>
2023-03-10 08:33:20 -0500

    added headshake animation to skills component

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,30 @@ module.exports = {
           '50%': { transform: 'rotate(15deg)' },
         },
 
+        headShake: {
+          '0%': {
+            transform: 'translateX(0)',
+          },
+          '6.5%': {
+            transform: 'translateX(-6px) rotateY(-9deg)',
+          },
+
+          '18.5%': {
+            transform: 'translateX(5px) rotateY(7deg)',
+          },
+
+          '31.5%': {
+            transform: 'translateX(-3px) rotateY(-5deg)',
+          },
+
+          '43.5%': {
+            transform: 'translateX(2px) rotateY(3deg)',
+          },
+          '50%': {
+            transform: 'translateX(0)',
+          },
+        },
+        
         jello: {
           'from, 11.1%,to': {
             transform: 'translate3d(0, 0, 0)',
@@ -48,6 +72,7 @@ module.exports = {
     animation: {
       wiggle: 'wiggle 1s ease-in-out infinite',
       jello: 'jello 2s infinite',
+      headShake: 'headShake 2s infinite',
     },
     fontFamily: {
       signature: ['Architects Daughter'],
