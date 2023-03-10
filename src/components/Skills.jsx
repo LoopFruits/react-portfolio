ba1d374b8f20d68dbb245dd8fed2ef1186d8c5c3
Kenneth Jordan <kensworldc@gmail.com>
2023-03-10 08:30:39 -0500

    adding animations for skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -68,7 +68,7 @@ const Skills = () => {
             skill.map(({id,title,src,style}) =>(
               <div 
               key={id}
-             className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+             className={`shadow-md  duration-500 hover:animate-jello py-2 rounded-lg  ${style}`}>
             <img src={src} alt="" className="w-20 mx-auto"></img>
             <p className="mt-4">{title}</p>
           </div>
